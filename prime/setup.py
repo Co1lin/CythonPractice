5762eb77d91c3c5df99d718637f218bde3832b71
wangwx <mail@co1in.me>
2022-01-17 18:29:36 +0800

    add prime function

---
+++ prime/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules = cythonize(
+        [
+            "primes.pyx",
+            'primes_python_compiled.py',
+        ],
+        annotate=True,
+    ),
+)
